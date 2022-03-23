339072f38545c00526bf81d4be69f312b4798670
lolo2438 <lolo2438@hotmail.com>
2022-03-22 21:15:33 -0400

    Update for serial communication

--- board_isr.cpp
+++ board_isr.cpp
@@ -3,19 +3,74 @@
 void board::limitsw_isr(void)
 {
     for(int i = 0; i < NB_MOTOR; i += 1) {
-        if(!limitsw[i].read())
-            disable_motor(i);
-    }
+        if(!limitsw[i].read()){
+
+            switch(i){
+            case 0:
+                dir = DIR_LEFT;
+                break;
+            case 1:
+                dir = DIR_RIGHT;
+                break;
+            case 2:
+                disable_pwm();
+                break;
+            case 3:
+                enable_pwm();
+                break;
+            }
+
+            //disable_motor(i);
+        }
+    }    
 }
 
+void board::rx_isr(void)
+{
+    char buf;
+    printer->read(&buf, sizeof(char));
+
+    // COMPATIBILITY FOR TESTS
+    //ucmd = (enum UART_CMD)buf;
+    //cmd_valid = true;
+
+    if(!cmd_valid){
+        uframe.cmd = (enum UART_CMD)buf;
+
+        switch(uframe.cmd){
+        case DATA0:
+        case DATA1:
+        case DATA2:
+        case DATA3:
+            cmd_valid = true;
+            break;
+        case HOME:
+        case RST_MOTOR:
+            cmd_valid = true;
+            rx_done = true;
+            break;
+        default:
+            break;
+            // Send error to printer...
+        }
+        
+    } else if(!rx_done) {
+        if(frame_cntr < 4){
+            uframe.data[frame_cntr++] = buf;
+        } else {
+            uframe.crc = buf;
+            rx_done = true;
+        }       
+    }
+}
 
 void board::step_cntr_isr(void)
 {
     nb_step += 1;
 
-    // For PPS = 200 && ustep=8, 100ms = 160 cnt
-    if(nb_step % 160 == 0)
-        led = !led;
+    // Step_limit can be set to 0;
+    if(step_limit && nb_step >= step_limit)
+        disable_pwm();
 
     //Need logit to sync reset
 }
--- main.cpp
+++ main.cpp
@@ -3,15 +3,130 @@
 
 BufferedSerial pc(USBTX,USBRX,9600);    //USBRX=PA_15, USB_TX=PA_2, no conflict
 
+//TODO: Add timers for overheat protection of motors
+
+board b;
+
+struct uart_frame data = {};
+
+enum UART_CMD reply;
+
+float requested_pos[NB_MOTOR] = {};
+float motor_pos[NB_MOTOR] = {};
+
+enum UART_CMD home_motors();
+enum UART_CMD reset_motors();
+
+
+/* Algorithme
+* 1. imprimante -> Reset
+* 2. Ack -> imprimante
+* 3. Imprimante -> P1
+* 4. Ack -> imprimante
+* 5. REPEAT for all points
+* 6. If moved(motors)
+*      RESAMPLE
+*    else
+*      DONE
+* ANYTIME: If error communication ERR, else ACK
+*/
 int main()
 {
-    board b;
-    b.wakeup_motors();
-
-    b.enable_all_motors();
-    b.enable_pwm();
+    b.led = 0;
     while (true) {
+        reply = CMD_NUL;
 
+        while(!b.has_rcv_uart());
+        data = b.read_uart();
+
+        switch(data.cmd){
+        case DATA0:
+        case DATA1:
+        case DATA2:
+        case DATA3:
+            // Verify CRC
+            requested_pos[data.cmd - DATA0] = *((float*)data.data);
+            reply = ACK;
+            break;
+        case HOME:
+            reply = home_motors();
+        case RST_MOTOR:
+            reply = reset_motors();
+            break;
+
+        default:
+            reply = ERR;
+            break;
+        }
+
+        if(reply != CMD_NUL){
+            b.uart_send(reply);
+        }
     }
 }
 
+
+/**
+* @fn home_motors()
+* @return RESAMPLE when motors have moved to the requested positino
+*         DONE     when no motors have moved since they are all stable
+* @brief
+*   Sets the motor position to the requested position by the
+*   printer.
+*/
+enum UART_CMD home_motors()
+{
+    bool motor_moved = false;
+    
+    const unsigned long STEP_TRESHOLD = 200; // ADJUST ME
+
+    b.disable_pwm();
+
+    for(unsigned i=0; i<NB_MOTOR; i+=1){
+        // TODO: calculate the number of steps needed to go to the position
+        unsigned long nb_step;
+        
+        if(nb_step >= STEP_TRESHOLD){
+            b.set_max_step(nb_step);
+            b.dir = DIR_RIGHT;
+            b.enable_motor(i);
+            b.enable_pwm();
+
+            while(b.is_motor_active(i)){
+                ThisThread::sleep_for(100ms);
+            }
+            
+            motor_moved = true;
+        } 
+    }
+    
+    return motor_moved ? RESAMPLE : DONE;
+}
+
+
+/**
+* @fn reset_motors()
+* @return DONE when all motors are offline
+* @brief 
+*   Sets the direction to LEFT and enables all motors.
+*   Then sleeps and wait until all motors are deactivated by
+*   the interrupt routine of the limit switches.
+*/
+enum UART_CMD reset_motors()
+{
+    b.dir = DIR_LEFT;
+    b.enable_all_motors();
+    b.enable_pwm();
+
+    while(b.nb_motors_active()){
+        ThisThread::sleep_for(100ms);
+    }
+
+    b.disable_pwm();
+    b.reset_position();
+
+    for(unsigned i=0; i<NB_MOTOR; i+=1)
+        motor_pos[i] = 0.0f;
+
+    return DONE;
+}
